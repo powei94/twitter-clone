59ac69e44ba3bc8fb9b340825c1f5f3dbd0dff12
Cristóbal <cristobal.tbecerraf@gmail.com>
2020-04-28 13:14:03 -0400

    vista feed y cambio nombre de columna en tweets

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_28_150110) do
+ActiveRecord::Schema.define(version: 2020_04_28_171014) do
 
   create_table "tweets", force: :cascade do |t|
-    t.text "tweet"
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
