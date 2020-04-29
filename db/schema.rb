c0c95d317dc99a543063d6e67976830895910c61
Cristóbal <cristobal.tbecerraf@gmail.com>
2020-04-29 14:22:38 -0400

    fix, ahora usuarios y correos se guardan correctamente

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_29_164022) do
+ActiveRecord::Schema.define(version: 2020_04_29_181309) do
 
   create_table "tweets", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
